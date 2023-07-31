7ddea70c1b56db7b6436d90928745468fd92344f
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-31 16:43:38 +0800

    Add SMS Alert feature

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
-from datetime import datetime, timedelta
 import requests
+from datetime import datetime, timedelta
+from twilio.rest import Client
 
 # DATA
 STOCK = "GOOGL"
 COMPANY_NAME = "Alphabet Inc Class A"
 ALPHA_VANTAGE_API_KEY = os.environ['ALPHA_VANTAGE_API_KEY']
 NEWS_API_KEY = os.environ['NEWS_API_KEY']
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_PHONE_NUMBER = '+14706135180'
+MY_PHONE_NUMBER = '+639561886073'
 
 stock_parameters = {
     'function': 'TIME_SERIES_DAILY',
@@ -33,10 +38,13 @@ difference = yesterday_closing - day_before_yesterday_closing
 percentage = round(difference / day_before_yesterday_closing * 100, 3)
 
 if percentage > 0:
+    stock_change = f"GOOGL: 🔺{percentage}%"
     print(f"GOOGL: 🔺{percentage}%")
 elif percentage == 0:
+    stock_change = f"GOOGL: 0%"
     print(f"GOOGLE: 0%")
 else:
+    stock_change = f"GOOGL: 🔻{percentage}%"
     print(f"GOOGL: 🔻{percentage}%")
 
 percentage = -5
@@ -50,12 +58,21 @@ if percentage >= 5 or percentage <= -5:
         'to': yesterday,
     }
     news_response = requests.get(url='https://newsapi.org/v2/everything', params=news_parameters)
-    print(news_response.json()['articles'][:3])
+    articles = news_response.json()['articles'][:3]
 
+# Load Client
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-
+for article in articles:
+    body = f"{stock_change}\n" \
+           f"Headline: {article['title']}\n" \
+           f"Brief: {article['description']}"
+    message = client.messages.create(
+        body=body,
+        from_=TWILIO_PHONE_NUMBER,
+        to=MY_PHONE_NUMBER
+    )
+    print(message.sid)
 
 # Optional: Format the SMS message like this:
 """
