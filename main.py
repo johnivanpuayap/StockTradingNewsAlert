52f59b2d6c6deea586724e8e386f63e450efe5eb
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-01 14:59:09 +0800

    Refactor the code

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ TWILIO_PHONE_NUMBER = '+14706135180'
 MY_PHONE_NUMBER = '+639561886073'
 articles = []
 
+# Request Data to the Alpha Vantage API
 stock_parameters = {
     'function': 'TIME_SERIES_DAILY',
     'symbol': STOCK,
@@ -21,22 +22,23 @@ stock_parameters = {
 }
 response = requests.get(url='https://www.alphavantage.co/query', params=stock_parameters)
 
-# Check the date today
+# Check the Date Today
 date_today = datetime.now().date()
 
 if date_today.weekday() == 0:
-    yesterday = (date_today - timedelta(days=3)).strftime("%Y-%m-%d")
-    day_before_yesterday = (date_today - timedelta(days=4)).strftime("%Y-%m-%d")
+    diff = 3
 else:
-    yesterday = (date_today - timedelta(days=1)).strftime("%Y-%m-%d")
-    day_before_yesterday = (date_today - timedelta(days=2)).strftime("%Y-%m-%d")
+    diff = 1
 
-yesterday_closing = float(response.json()['Time Series (Daily)'][yesterday]['4. close'])
-day_before_yesterday_closing = float(response.json()['Time Series (Daily)'][day_before_yesterday]['4. close'])
+yesterday = (date_today - timedelta(days=diff)).strftime("%Y-%m-%d")
+day_before_yesterday = (date_today - timedelta(days=diff + 1)).strftime("%Y-%m-%d")
 
-# Calculate the change
-difference = yesterday_closing - day_before_yesterday_closing
-percentage = round(difference / day_before_yesterday_closing * 100, 3)
+yesterday_closing_price = float(response.json()['Time Series (Daily)'][yesterday]['4. close'])
+day_before_yesterday_closing_price = float(response.json()['Time Series (Daily)'][day_before_yesterday]['4. close'])
+
+# Calculate the Change
+difference = yesterday_closing_price - day_before_yesterday_closing_price
+percentage = round(difference / day_before_yesterday_closing_price * 100, 3)
 
 if percentage > 0:
     stock_change = f"GOOGL: 🔺{percentage}%"
@@ -45,7 +47,7 @@ elif percentage == 0:
 else:
     stock_change = f"GOOGL: 🔻{percentage}%"
 
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# Get News when percentage is +-5%
 if percentage >= 5 or percentage <= -5:
     news_parameters = {
         'q': "google",
@@ -56,9 +58,10 @@ if percentage >= 5 or percentage <= -5:
     news_response = requests.get(url='https://newsapi.org/v2/everything', params=news_parameters)
     articles = news_response.json()['articles'][:3]
 
-# Load Client
+# Load Client to Send SMS
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
+# Send SMS for every article
 for article in articles:
     body = f"{stock_change}\n" \
            f"Headline: {article['title']}\n" \
